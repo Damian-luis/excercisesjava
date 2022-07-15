95d97683abe1b4abeb97c10a4de99b1ca86d5109
Damian-luis <damian.luis.porta@gmail.com>
2022-07-14 22:47:50 -0300

    first commit

---
+++ src/excercises/first.java
@@ -0,0 +1,14 @@
+package excercises;
+
+public class first {
+    public static void main(String[] args){
+        int entero=1;
+        long grande=4846845;
+        double decimal=1890;
+        String string="example";
+        System.out.println(entero);
+        System.out.println(grande);
+        System.out.println(decimal);
+        System.out.println(string);
+    }
+}
