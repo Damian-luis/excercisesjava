9501a727a06bd4be0d89f0d07f9ab396447e143f
Damian-luis <damian.luis.porta@gmail.com>
2022-07-15 00:41:10 -0300

    third commit

---
+++ src/excercises/third.java
@@ -0,0 +1,17 @@
+package excercises;
+
+public class third {
+    public static void main(String[] args){
+        String [] names = {"damian"," tomi"," coki", " nico"," lola"," lucky"};
+        String finalNames="";
+
+        for (int i=0;i<names.length;i++){
+
+            finalNames+=names[i];
+
+        }
+        System.out.println(finalNames);
+
+
+    }
+}
